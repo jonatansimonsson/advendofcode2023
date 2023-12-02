491158418d9ed4ad2ced6319bf2b8544a138a66e
Jonatan Simonsson <jonsih181@edunet.oru.se>
2023-12-02 07:56:42 +0100

    Update solution to day1 for cleaness

--- day01/day1.php
+++ day01/day1.php
@@ -10,23 +10,30 @@ $content = file_get_contents($file);
 $puzzle = 2;
 $line = preg_split("/\n/", $content);
 
+$toBeReplaced = array("one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
+$replacement = array("o1e", "t2o", "3te", "f4r", "f5e", "s6x", "s7n", "e8t", "n9e");
+
 $sum = 0;
+
 foreach($line as $l) {
+
     $numbers = array();
+
     if ($puzzle == 2){
-        $l = str_replace("one", "one1one", $l); $l = str_replace("two", "two2two",$l); $l = str_replace("three", "three3three",$l);
-        $l = str_replace("four", "four4four",$l); $l = str_replace("five", "five5five",$l); $l = str_replace("six", "six6six",$l);
-        $l = str_replace("seven", "seven7seven",$l); $l = str_replace("eight", "eight8eight",$l); $l = str_replace("nine", "nine9nine",$l);
+        $l = str_replace($toBeReplaced, $replacement, $l);
     }
+
     for($c = 0; $c < strlen($l); $c++) {
         if(is_numeric($l[$c])) {
             array_push($numbers, $l[$c]);
         }
     }
+
     if($numbers) {
         $sum += (int)(reset($numbers) . end($numbers));
     }    
 }
+
 print("Result of Part " . $puzzle . " is: " . $sum);
 
 
